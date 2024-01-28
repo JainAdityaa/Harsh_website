20e942503cce1eed6dff8dc76c90910d026d6624
Paras Kumar <paraskumar@Parass-MacBook-Air.local>
2024-01-28 23:30:27 +0530

    animation added

--- src/Components/Touch.js
+++ src/Components/Touch.js
@@ -7,8 +7,17 @@ export const Touch = () => {
             <div className='container'>
                 <div className='row'>
                     <h5 className='touch-title'>Time to stop scrolling</h5>
-                    <div className='row justify-content-center pb-2'>
-                        <a href='#' className='text-center'><img className='touch-img' src='./images/getintouch-logo.png' /></a>
+                    <div className='row justify-content-center pb-2 mc-animation-wrapper'>
+                        <div className='mc-animation-container'>
+                            <a href='#' className='text-center'>
+                                <div className='mc-animation-outer-ring'>
+                                    <img className='touch-img' src='./images/getintouch-logo.png' />
+                                </div>
+                            </a>
+                            <div className='mc-animation-waves'>
+                                <div className='mc-animation-inner-wave'></div>
+                            </div>
+                        </div>
                     </div>
                     <p className='touch-text'><a href='#'>Get In Touch</a></p>
                 </div>
